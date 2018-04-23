722b23ba0219ebbf4086ff43c7a0479352cb3e82
Vincent Luong <luongv4@uci.edu>
2018-04-22 19:00:23 -0700

    added calendar and calendar page
    gets date time of the actual day and transfers to html

--- website/mainPage/views.py
+++ website/mainPage/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from .models import Organization, Event
+import datetime
+import calendar
 
 def index(request):
     events = Event.objects.all()
     return render(request,'mainPage/index.html',{'events': sorted(events,key = lambda x: x.name)})
+
+def calendarPage(request):
+    year = datetime.date.today().year
+    month = datetime.date.today().month
+    day = datetime.date.today().day
+    events = Event.objects.all()
+    return render(request,'mainPage/calendarPage.html',
+    {'events': sorted(events,key = lambda x: x.name),
+    'name_of_month' :calendar.month_name[month],
+    'num_days': range(calendar.monthrange(year,month)[1]),
+    'year':year,
+    'day' : day,
+    'name_of_day' : datetime.datetime.now().strftime("%A")})
