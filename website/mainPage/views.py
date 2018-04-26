35844e0e0a4845ff75f1e1fdc68624bd6e56d868
Vincent Luong <luongv4@uci.edu>
2018-04-25 21:30:06 -0700

    Fixed Calendar and added mini Calendar
    allow for change of month by changing view
    allow scroll for events on calendar
    
    Co-Authored-By: javidjs2500 <javidjs2500@users.noreply.github.com>
    Co-Authored-By: adrienne caparaz <adriennecaparaz@users.noreply.github.com>

--- website/mainPage/views.py
+++ website/mainPage/views.py
@@ -7,16 +7,19 @@ from collections import defaultdict
 def index(request):
     event_dict = defaultdict(list)
     events = Event.objects.all()
-    # trying dictionary
+    # dictionary key = dates, values = list of events
     for ev in sorted(events,key = lambda x: x.date_time):
-        event_dict[str(ev.date_time.strftime("%A")) + ' '+str(ev.date_time.day) +', '+ str(ev.date_time.year)].append(ev)
+        event_dict[str(ev.date_time.strftime("%A"))+ ', ' + str(ev.date_time.strftime("%B")) + ' '+str(ev.date_time.day) +', '+ str(ev.date_time.year)].append(ev)
     month = {1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December"}
     return render(request,'mainPage/index.html',{'ev_dict': dict(event_dict), "months": month})
 
 def calendarPage(request):
-    year = datetime.date.today().year
-    month = datetime.date.today().month
-    day = datetime.date.today().day
+    today_date = datetime.datetime.today()
+    first_day = {'Sunday': 1,'Monday': 2,'Tuesday': 3,'Wednesday': 4,'Thursday': 5,'Friday': 6,'Saturday': 7}[today_date.replace(day=1).strftime("%A")]
+    print(first_day)
+    year = today_date.year
+    month = today_date.month
+    day = today_date.day
     events = Event.objects.all()
     return render(request,'mainPage/calendarPage.html',
     {'events': sorted(events,key = lambda x: x.name),
@@ -25,4 +28,5 @@ def calendarPage(request):
     'cur_year':year,
     'cur_month':month,
     'cur_day' : day,
-    'cur_name_of_day' : datetime.datetime.now().strftime("%A")})
+    'cur_name_of_day' : datetime.datetime.now().strftime("%A"),
+    'first_day':range(first_day-1)})
