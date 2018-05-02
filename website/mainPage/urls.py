57bba49bcb6c3af350af34b9b83cea2da9d95457
Javid Singh <javidjs@uci.edu>
2018-05-02 16:25:56 -0700

    Converting to multi-user platform
    Changed how incrementing/decrementing months worked. Moved css into a different file and directory to make code cleaner.
    
    Co-Authored-By: vincentluong13 <vincentluong13@users.noreply.github.com>

--- website/mainPage/urls.py
+++ website/mainPage/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = "mainPage"
 urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('calendarpage/',views.calendarPage , name = 'calendar'),
-    path('calendarpage/nextMonth/', views.nextMonth, name = 'nextMonth'),
-    path('calendarpage/prevMonth/', views.prevMonth, name = 'prevMonth'),
-    path('calendarpage/curDay/', views.resetToCurrent, name = 'currentTime'),
+    path('', views.index, name = 'index'), #/, the 'home page', view of the schedule
+    path('calendarpage/',views.calendarPage , name = 'calendar'), #calendarpage/, the current month's view of the calendar page
+    path('calendarpage/curDay/', views.resetToCurrent, name = 'currentTime'), #resets the day to today, then redirects to calendarpage/
+    re_path(r'^(?P<view>month|week|year)\/(?P<year>[1-9]?[0-9]{3})-(?P<month>[0-1]?[0-9])-(?P<day>[0-3]?[0-9])/$', views.cal, name = 'cal'), #month/2018-10-3, changes the current view using the desired view type, and the date. Uses calendarpage.html to create the calendar
+    path('calendarpage/nextView/<str:view>/<int:year>/<int:month>/<int:day>/', views.nextView, name = "nextView"), #calendarpage/nextView/month/2018/10/3, uses the nextView function to increment the view type to display the next view, then redirects the link to show the updated view
+    path('calendarpage/prevView/<str:view>/<int:year>/<int:month>/<int:day>/', views.prevView, name = "prevView"), #calendarpage/prevView/week/2018/10/3, uses thep prevView function to decrement the view type to display the next view, then redirects thel ink tos how the updated view 
 ]
--- website/mainPage/views.py
+++ website/mainPage/views.py
@@ -13,7 +13,12 @@ global cal_month
 global cal_year
 cal_year = datetime.datetime.today().year
 cal_month  = datetime.datetime.today().month
+
 def index(request):
+    '''
+    The index is basically the main page of our website. It creates a dictionary of events and then sorts all events by date. It sends all the events 
+    to be viewed in a schedule-type view.
+    '''
     event_dict = defaultdict(list)
     events = Event.objects.all()
 
@@ -28,42 +33,75 @@ def index(request):
     
 
 def calendarPage(request):
-    global cal_month
+    '''
+    This calendar page is the base calendar page. Redirecting to this page will bring you to the calendar view with default view month and will show you
+    the current month. It works by taking in events that only take place in the current month, the amount of days in the month, and which day the month starts on
+    and then sends those events to calendarPage.html to create the calendar view.
+    '''
 
-    cal_var = get_calendar_variables(c_month = cal_month, c_year = cal_year)
+    today_date = datetime.datetime.today()
+    cal_var = get_calendar_variables(c_month = today_date.month, c_year = today_date.year) #retrieves all the variables for the current calendar month/year
 
-    
+    data = {'n_view': 'month', 'n_year': today_date.year, 'n_month': today_date.month, 'n_day': today_date.day} #makes a dictionary of the current day's month/day/year, needed to view the next month/week/year
     events = Event.objects.filter(date_time__month = cal_var["cur_month"])
-    insert_dict = {'events': sorted(events,key = lambda x: x.date_time)}
-    insert_dict.update(cal_var)
-    return render(request,'mainPage/calendarPage.html',insert_dict)
 
-def nextMonth(request):
-    global cal_month
-    global cal_year
+    insert_dict = {'events': sorted(events,key = lambda x: x.date_time)} #creates a dictionary with the events in the current month
 
-    if cal_month == 12:
-        cal_month = 1
-        cal_year += 1
-    else:
-        cal_month += 1
+    insert_dict.update(cal_var) #adds the calendar variables into the dictionary that will passed onto html
+    insert_dict.update(data) #adds needed data into the dictionary that will passed onto html
 
-    return HttpResponseRedirect('/calendarpage')
 
-def prevMonth(request):
-    global cal_month
-    global cal_year
-    if cal_month == 1:
-        cal_month = 12
-        cal_year -= 1
-    else:
-        cal_month -= 1
+    return render(request,'mainPage/calendarPage.html',insert_dict) #creates the calendar view using the insert dict
 
-    return HttpResponseRedirect('/calendarpage')
+def cal(request, view, year, month, day):
+    '''
+    The cal view is used for displaying views on the calendar using specific dates in the past or in the future.
+    It works by taking in the desired view type, the year, the month, and the day, and then does the same thing calendarPage does except
+    it uses the variables for the desired month/year/week instead of the the current date's variables.
+    '''
+    if view == 'month':
+        cal_var = get_calendar_variables(c_month = int(month), c_year = int(year)) #creates a cal_var using the desired month/year
+
+        events = Event.objects.filter(date_time__month = cal_var["cur_month"])
+        insert_dict = {'events': sorted(events,key = lambda x: x.date_time)}
+        insert_dict.update(cal_var)
+        data = {'n_view': view, 'n_year': year, 'n_month': month, 'n_day': day}
+        insert_dict.update(data)
+    
+    return render(request,'mainPage/calendarPage.html', insert_dict)
+
+def nextView(request, view, year, month, day):
+    '''
+    The nextView function is used to increment views. It takes in the parameters view, year, month, and day and will increment the days/month/year depending on 
+    the desired view.
+    '''
+    if view == 'month':
+        if month == 12: #makes sure that if the month is December, it will increment the year by 1 and set the month to January
+            month = 1
+            year += 1
+        else:
+            month += 1
+        day = 1 #sets the day to 1 just to be sure that the date is a valid day
+
+    return HttpResponseRedirect('/' + str(view) + '/' + str(year) + '-' + str(month) + '-' + str(day) + '/') #month/2018-06-12 -> month/2018/07/01
+
+def prevView(request,view, year, month, day):
+    '''
+    The prevView function is used to decrement views. It takes in the parameters view, year, month, and day and will decrement the days/month/year depending on
+    the desired view
+    '''
+    if view =='month':  #makes sure that if the month is January, it will decrement the year by 1 and set the month to December  
+        if month == 1:
+            month = 12
+            year -= 1
+        else:
+            month -= 1
+        day = 1 #makes sure that the day is a vali date
+
+    return HttpResponseRedirect('/' + str(view) + '/' + str(year) + '-' + str(month) + '-' + str(day) + '/') #month/2018-06-12 -> month/2018-05-01
 
 def resetToCurrent(request):
-    global cal_month
-    global cal_year
-    cal_year = datetime.datetime.today().year
-    cal_month  = datetime.datetime.today().month
+    '''
+    This function redirects to the calendarpage to reset the date to view the current month/week/year
+    '''
     return HttpResponseRedirect('/calendarpage')
